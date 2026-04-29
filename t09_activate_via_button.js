ebdb7f4072cf37b92a12b67fa0ecb372c63a0918
24096ao <24096ao@hvhs.school.nzgit switch main>
2026-04-29 18:32:30 +1200

    continue t10

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -3,23 +3,25 @@ t09_activate_via_buttons.js
 ****************************/
 console.log("t09_activate_via_buttons.js")
 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 
-// variables
-let name = "Bob";
-let age = 27;
-let pocketMoney = 5;
-let year = 2059;
+    // variables
+    let name = "Bob";
+    let age = 27;
+    let pocketMoney = 5;
+    let year = 2059;
 
 /****************************
 Main code
 ****************************/
-start();
+
 
 /****************************
 functions
 ****************************/
 function start() {
+    console.log("Start")
+
     welcome();
     displayWelcome("Jhon",107)
     displayProduct("chocolate Bar", 4)
@@ -28,19 +30,19 @@ function start() {
     OUTPUT.innerHTML += "<p>The current year is "+ year + "</p>";
     OUTPUT.innerHTML += "<p>if you spent half your money you would have ", pocketMoney/2, "dollars</p>";
     OUTPUT.innerHTML += "<p>Then you get $3 you now have ", pocketMoney+3, "dollars</p>";
+}
 
-    function welcome() {
-        OUTPUT.innerHTML = "<h1>Welcome to the shop!</h1>";
-    }
+function welcome() {
+    OUTPUT.innerHTML = "<h1>Welcome to the shop!</h1>";
+}
 
-    function displayWelcome(_name, _age) {
-        OUTPUT.innerHTML += "<h1>Hi " +_name + "</h1>";
-        OUTPUT.innerHTML += "<p>you are " +_age + " years old</p>";
-    }
+function displayWelcome(_name, _age) {
+    OUTPUT.innerHTML += "<h1>Hi " +_name + "</h1>";
+    OUTPUT.innerHTML += "<p>you are " +_age + " years old</p>";
+}
 
-    function displayProduct(_productName,_price) {
-        OUTPUT.innerHTML += "<p>" +_productName + ": $" +_price + "</p>";
-    }
+function displayProduct(_productName,_price) {
+    OUTPUT.innerHTML += "<p>" +_productName + ": $" +_price + "</p>";
 }
 
 
--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,57 @@
-/* Paste your code from the last task */
+/****************************
+t10_input_from_HTML.js
+****************************/
+console.log("t10_input_from_HTML.js")
+
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+
+    //var
+    let age = 27;
+    let pocketMoney = 5;
+    let year = 2059;
+
+/****************************
+Main code
+****************************/
+
+
+/****************************
+functions
+****************************/
+function start() {
+    console.log("Start")
+
+    welcome();
+    displayWelcome("Jhon",107)
+    displayProduct("chocolate Bar", 4)
+    displayProduct("Chips", 3)
+    displayProduct("Drink", 2.50)
+    OUTPUT.innerHTML += "<p>The current year is "+ year + "</p>";
+    OUTPUT.innerHTML += "<p>if you spent half your money you would have ", pocketMoney/2, "dollars</p>";
+    OUTPUT.innerHTML += "<p>Then you get $3 you now have ", pocketMoney+3, "dollars</p>";
+}
+
+function welcome() {
+    OUTPUT.innerHTML = "<h1>Welcome to the shop!</h1>";
+}
+
+function displayWelcome(_name, _age) {
+    OUTPUT.innerHTML += "<h1>Hi " +_name + "</h1>";
+    OUTPUT.innerHTML += "<p>you are " +_age + " years old</p>";
+}
+
+function displayProduct(_productName,_price) {
+    OUTPUT.innerHTML += "<p>" +_productName + ": $" +_price + "</p>";
+}
+
+function getFormInput() {
+    const NAME_FIELD = DocumentFragment.getElementById("nameField")
+    let name = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+ name + "</p>"
+}
+
+
+/****************************
+end of code
+****************************/
+
