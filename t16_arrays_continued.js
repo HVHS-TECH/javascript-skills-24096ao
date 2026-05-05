ac7fd2fddf916e21c643abeb73f225d0406d7835
24096ao <24096ao@hvhs.school.nzgit switch main>
2026-05-06 11:38:15 +1200

    continue t16

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -3,23 +3,14 @@ t16_arrays_continued.js
 ****************************/
 console.log("t16_arrays_continued.js")
 
-const CHOICE_FIELD = document.getElementById("choiceField")
+const ITEM_FIELD = document.getElementById("itemField")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+
+let classArray = ["alice", "Bob", "jhon", "kaia", "olivia"]
 
 /****************************
 Main code
 ****************************/
-function start() {
-    let choice = Number(CHOICE_FIELD.value);
-    
-    console.log("started")
-    console.log(choice)
-    
-    for (let i=choice; i>=0; i--) {
-        console.log(i+ " bottles of bear on the wall")
-    }
-    
-    console.log("Theres no more bears :(")
-}
 
 
 /****************************
