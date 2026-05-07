33cc32f4e653039068824d87bc8ba0ef997cccc4
24096ao <24096ao@hvhs.school.nzgit switch main>
2026-05-07 13:29:10 +1200

    Continue t16

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -6,20 +6,25 @@ console.log("t16_arrays_continued.js")
 const ITEM_FIELD = document.getElementById("itemField")
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 
-let classArray = ["alice", "Bob", "jhon", "kaia", "olivia"]
+let classArray = ["Alice", " Bob", " jhon", " kaia", " olivia"]
+let item = ITEM_FIELD.value;
 
 /****************************
 Main code
 ****************************/
-
+addItem()
+console.log("in your shopping list is " +classArray)
 
 /****************************
 functions
 ****************************/
-
-
-
+function addItem() {
+    for (let i=0; i<1; i++) {
+    classArray.push(" " +item)
+    }
+}
 /****************************
 end of code
 ****************************/
+
 
